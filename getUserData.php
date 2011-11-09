bec11bd7455297df59a8592c8bf7f6f78b9fafe6
Kartikeya <kartikeyadubey@gmail.com>
2011-11-09 10:05:58 -0500

    Trial pulling from database

---
+++ getUserData.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Pull data for a given id from the database
+ */
+
+$id = isset($_GET["id"])? $_GET["id"]:"";
+
+mysql_connect("localhost","root","");
+mysql_select_db("getitback");
+
+$result = mysql_query("SELECT description, date, personTwo, personTwoName,money FROM money where personOne='".$id."'");
+
+$personData = array();
+$personData[$id] = array();
+
+while($row = mysql_fetch_assoc($result))
+{
+	$temp = array();
+	$temp["description"] = $row["description"];
+	$temp["date"] = $row["date"];
+	$temp["personTwoName"] = $row["personTwoName"];
+	$temp["money"] = $row["money"];
+	array_push($personData[$id], $temp);
+}
+
+mysql_close();
+	
+echo json_encode($personData);
+
+?>
